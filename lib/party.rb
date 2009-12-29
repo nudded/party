67cc1cfa46f3f4437f4bc2c569ff0e6c20ef2390
Toon Willems <willemstoon@gmail.com>
2009-12-29 19:08:12 +0100

    now everything is in a module

--- lib/party.rb
+++ lib/party.rb
@@ -1,67 +1,76 @@
-def participants(*args)
-  @participants ||= args.flatten
-end
+module Party
 
+  def participants(*args)
+    @participants ||= args.flatten
+  end
 
-def bought(name,item, price)
-  list[name] << item if item
-  money[name] += price
-end
+  def bought(name,item, price)
+    list[name] << item if item
+    money[name] += price
+  end
 
-alias bought2 bought
-# monkey-patches String class to have a buys method
-def fancy_mode
-  # some minor modifications to the String class ;) 
-  String.class_eval do
-    define_method(:bought) do |*args|
-      bought2(self,*args)
+  def pays(name,to, amount)
+
+  end
+
+  alias bought2 bought
+  # monkey-patches String class to have a buys method
+  def fancy_mode
+    # some minor modifications to the String class ;) 
+    String.class_eval do
+      define_method(:bought) do |*args|
+        bought2(self,*args)
+      end
     end
   end
-end
 
-def summarize(rounding=2)
-  print_bill
-  puts
-  print_money_due(rounding)
-end
+  def summarize(rounding=2)
+    print_bill
+    puts
+    print_money_due(rounding)
+  end
 
-private
+  private
 
-def print_money_due(rounding)
-  puts "Final balance:"
-  participants.each do |par|
-    diff = cost_per_participant - money[par]
-    print par
-    if diff > 0
-      print " needs to pay "
-    else
-      print " has to receive "
+  def print_money_due(rounding)
+    puts "Final balance:"
+    participants.each do |par|
+      diff = cost_per_participant - money[par]
+      print par
+      if diff > 0
+        print " needs to pay "
+      else
+        print " has to receive "
+      end
+      puts "#{diff.abs.round(rounding)} euro"
     end
-    puts "#{diff.abs.round(rounding)} euro"
   end
-end
 
-def print_bill
-  puts "---Bill---"
-  money.keys.each do |par|
-    puts "#{par}:\t#{money[par]}\tspend on: #{list[par] * ',' }"
+  def print_bill
+    puts "---Bill---"
+    money.keys.each do |par|
+      puts "#{par}:\t#{money[par]}\tspend on: #{list[par] * ',' }"
+    end
+    puts "----------"
+    puts "total:\t#{total_cost}"
   end
-  puts "----------"
-  puts "total:\t#{total_cost}"
+
+  def cost_per_participant
+    total_cost.to_f/participants.size
+  end
+
+  def total_cost
+    money.values.inject(&:+) 
+  end
+
+  def money
+    @@money_hash ||= Hash.new(0)
+  end
+
+  def list
+    @@list_hash ||= Hash.new() {|h,k| h[k] = []}
+  end
+  
 end
 
-def cost_per_participant
-  total_cost.to_f/participants.size
-end
-
-def total_cost
-  money.values.inject(&:+) 
-end
-
-def money
-  @@money_hash ||= Hash.new(0)
-end
-
-def list
-  @@list_hash ||= Hash.new() {|h,k| h[k] = []}
-end
+include Party
